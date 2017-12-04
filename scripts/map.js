40643981f1e3d416cbaa5e4d837ac237c332240c
Christian Höglund <chrhog@pc.symsoft.se>
2017-12-04 12:42:54 +0100

    small screen fixes 2

--- scripts/map.js
+++ scripts/map.js
@@ -18,11 +18,11 @@ var per_albin_hansson_contentString =
 	'<div id="content">'+
 		'<div id="siteNotice">'+
 		'</div>'+
-		'<a href="pages/per_albin_hansson.html"><h4 id="firstHeading" class="firstHeading">Per Albin Hansson</h4></a>'+
+		'<h4 id="firstHeading" class="firstHeading">Per Albin Hansson</h4>'+
 		'<div id="bodyContent">'+
 			'<div class="infoviewImage" style="float:left; width:160px;"><a href="pages/per_albin_hansson.html"><img src="pics/per_albin_hansson/256px-Byst_av_nämndeman_Per_Albin_Hansson,_av_Emil_Näsvall.JPG" alt="Per Albin Hansson" style="width:150px; align:center;"></a></div>' +
 			'<div style="paddingLeft:5px"><p>Byst av <b>Per Albin Hansson</b> i Parken Zoo i Eskilstuna</p>'+
-			'<p>Klicka på bilden för mer information</p>' +
+			'<p>Klicka <a href="pages/per_albin_hansson.html">här</a> för mer information</p>' +
 			'<p><a href="https://www.google.com/maps/search/?api=1&query=59.372830,16.482146">Hitta hit</a></p></div>'+
 		'</div>'+
 	'</div>';
@@ -34,12 +34,12 @@ var maj_danderyd_contentString =
 	'<div id="content">'+
 		'<div id="siteNotice">'+
 		'</div>'+
-		'<a href="pages/maj.html"><h4 id="firstHeading" class="firstHeading">Maj</h4></a>'+
+		'<h4 id="firstHeading" class="firstHeading">Maj</h4>'+
 		'<div id="bodyContent">'+
 			'<div class="infoviewImage" style="float:left; width:160px;"><a href="pages/maj.html"><img src="pics/maj-danderyd/256px-Emil_Näsvall_Maj.JPG" alt="Maj" style="width:150px; align:center;"></a></div>' +
 			'<div style="paddingLeft:5px"><p>Statyn <b>Maj</b> i Danderyd är en bronsstaty, ca. två meter hög på en marmorsockel.</p>'+
 			'<p>Maj finns även i Strykjärnsparken, Söderhamn och Skolparken, Vilhelmina</p>' +
-			'<p>Klicka på bilden för mer information</p>' +
+			'<p>Klicka <a href="pages/maj.html">här</a> för mer information</p>' +
 			'<p><a href="https://www.google.com/maps/search/?api=1&query=59.395572,18.037009">Hitta hit</a></p></div>'+
 		'</div>'+
 	'</div>';
@@ -54,8 +54,7 @@ var harjedalens_fjallmuseum_contentString =
 		'<div id="bodyContent">'+
 			'<p><b>Härjedalens Fjällmuseum</b> har en permanent utställning av ' +
 			'en del av <b>Emil Näsvalls</b> verk.</p>'+
-			'<p>Fjällmuseets hemsida är <a href="http://kommun.herjedalen.se/fjallmuseet.4.7a231afd12424b40e2880001097.html">' +
-			'http://kommun.herjedalen.se/fjallmuseet.4.7a231afd12424b40e2880001097.html</a></p>'+
+			'<p>Fjällmuseets hemsida hittar du <a href="http://kommun.herjedalen.se/fjallmuseet.4.7a231afd12424b40e2880001097.html">här</a></p>'+
 			'<p><a href="https://www.google.com/maps/search/?api=1&query=62.548326,12.537803">Hitta hit</a></p>'
 		'</div>'+
 	'</div>';
@@ -67,11 +66,11 @@ var harjedalens_fjallmuseum_contentString =
 		'<div id="content">'+
 			'<div id="siteNotice">'+
 			'</div>'+
-			'<a href="pages/i_sommarhagen.html"><h4 id="firstHeading" class="firstHeading">I sommarhagen</h4></a>'+
+			'<h4 id="firstHeading" class="firstHeading">I sommarhagen</h4>'+
 			'<div id="bodyContent">'+
 				'<div class="infoviewImage" style="float:left; width:160px;"><a href="pages/i_sommarhagen.html"><img src="pics/i_sommarhagen/256px-I_sommarhagen.jpg" alt="Maj" style="width:150px; align:center;"></a></div>' +
 				'<div style="paddingLeft:5px"><p>Statyn <b>I sommarhagen</b> står mellan två hus i Varberg</p>'+
-				'<p>Klicka på bilden för mer information</p>' +
+				'<p>Klicka <a href="pages/i_sommarhagen.html">här</a> för mer information</p>' +
 				'<p><a href="https://www.google.com/maps/search/?api=1&query=57.099466,12.248345">Hitta hit</a></p></div>'+
 			'</div>'+
 		'</div>';
@@ -88,11 +87,11 @@ var locations = [
 	{lat: 59.395572, lng: 18.037009, title: "Maj i Danderyd", info: maj_danderyd_contentString, pinColor: null},
 	{lat: 62.548326, lng: 12.537803, title: "Härjedalens Fjällmuseum", info: harjedalens_fjallmuseum_contentString,  pinColor: "007569"},
 	{lat: 59.372830, lng: 16.482146, title: "Per Albin Hansson", info: per_albin_hansson_contentString, pinColor: null},
-	{lat: 57.099466, lng: 12.248345, title: "I sommarhagen", info: i_sommarhagen_contentString, pinColor: null},
-	{lat: 59.594572, lng: 18.137009, title: "1", info: null, pinColor: null},
-	{lat: 59.494572, lng: 18.237009, title: "2", info: null, pinColor: null},
-	{lat: 59.294572, lng: 18.337009, title: "3", info: null, pinColor: null},
-	{lat: 59.694572, lng: 18.437009, title: "4", info: null, pinColor: null}
+	{lat: 57.099466, lng: 12.248345, title: "I sommarhagen", info: i_sommarhagen_contentString, pinColor: null}//,
+	//{lat: 59.594572, lng: 18.137009, title: "1", info: null, pinColor: null},
+	//{lat: 59.494572, lng: 18.237009, title: "2", info: null, pinColor: null},
+	//{lat: 59.294572, lng: 18.337009, title: "3", info: null, pinColor: null},
+	//{lat: 59.694572, lng: 18.437009, title: "4", info: null, pinColor: null}
 ];
 
 
