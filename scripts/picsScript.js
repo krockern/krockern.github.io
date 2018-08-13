405387732785ddff9254ab18adddf760fc50ee84
Christian Höglund <hoglund.c@gmail.com>
2018-08-13 15:43:53 +0200

    Made a more generic slideshow

--- scripts/picsScript.js
+++ scripts/picsScript.js
@@ -96,8 +96,12 @@ function picsGallery(rowSize) {
  * Slideshows
 ***************************/
 
-function largescreenPics() {
-  var largescreenDiv = document.getElementById("largescreen");
+function slideshow(rowSize) {
+
+  if (rowSize == 4) { var screenDiv = document.getElementById("largescreen"); }
+  else if (rowSize == 3) { var screenDiv = document.getElementById("mediumscreen"); }
+  else if (rowSize == 1) { var screenDiv = document.getElementById("smallscreen"); }
+
   var col = 1;
   var row = 0;
   var newrow = true;
@@ -111,8 +115,11 @@ function largescreenPics() {
     //Create slide div
     if (slide) {
       slideDiv = document.createElement("div");
-      slideDiv.className += "slideDiv_large fade";
-      largescreenDiv.appendChild(slideDiv);
+      if (rowSize == 4) { slideDiv.className += "slideDiv_large fade"; }
+      else if (rowSize == 3) { slideDiv.className += "slideDiv_medium fade"; }
+      else if (rowSize == 1) { slideDiv.className += "slideDiv_small fade"; }
+
+      screenDiv.appendChild(slideDiv);
 
       slide = false;
     }
@@ -132,7 +139,7 @@ function largescreenPics() {
 
     }
       var picDiv1 = document.createElement("div");
-      picDiv1.className += "w3-col l3 m6 w3-margin-bottom";
+      picDiv1.className += "w3-col l3 m4 w3-margin-bottom";
       rowDiv.appendChild(picDiv1);
 
       var picDiv2 = document.createElement("div");
@@ -159,7 +166,7 @@ function largescreenPics() {
       anchor.appendChild(image);
 
       //End row and do one more
-      if (col == 4) {
+      if (col == rowSize) {
         //Only do new row if there is more pics in the list
         if (index < (picsList.length - 1)) {
           newrow = true;
@@ -172,108 +179,4 @@ function largescreenPics() {
       }
       col++;
   }
-}
-
-function mediumscreenPics() {
-  var mediumscreenDiv = document.getElementById("mediumscreen");
-  var col = 1;
-  var row = 0;
-  var newrow = true;
-  var slide = true;
-
-
-  for (let index = 0; index < picsList.length; ++index) {
-    var rowDiv;
-    var slideDiv;
-
-    //Create slide div
-    if (slide) {
-      slideDiv = document.createElement("div");
-      slideDiv.className += "slideDiv_medium fade";
-      mediumscreenDiv.appendChild(slideDiv);
-
-      slide = false;
-    }
-
-    //Create new row
-    if (newrow) {
-      rowDiv = document.createElement("div");
-      rowDiv.className += "w3-row-padding";
-      slideDiv.appendChild(rowDiv);
-
-      //reset counters
-      col = 1;
-      row++;
-
-      //new row created set newrow to false
-      newrow = false;
-
-    }
-      var picDiv1 = document.createElement("div");
-      picDiv1.className += "w3-col l3 m4 w3-margin-bottom"; //was m6
-      rowDiv.appendChild(picDiv1);
-
-      var picDiv2 = document.createElement("div");
-      picDiv2.className += "w3-display-container";
-      picDiv1.appendChild(picDiv2);
-
-      var picDiv3 = document.createElement("div");
-      picDiv3.className += "w3-display-topleft w3-black w3-padding"; // w3-black
-      picDiv3.setAttribute("style", "color:#fff!important;background-color:rgba(0,0,0,0.5)!important");
-      picDiv3.appendChild(document.createTextNode(picsList[index][0]));
-      picDiv2.appendChild(picDiv3);
-
-      var anchor = document.createElement('a');
-      anchor.setAttribute('href', picsList[index][1]);
-      picDiv2.appendChild(anchor);
-
-      //Image
-      var image = document.createElement("img");
-      image.className += picsList[index][2];
-      image.setAttribute("src", picsList[index][3]);
-      image.setAttribute("alt", picsList[index][4]);
-      image.setAttribute("style", picsList[index][5]);
-
-      anchor.appendChild(image);
-
-      //End row and do one more
-      if (col == 3) { //with m6 use 2
-        //Only do new row if there is more pics in the list
-        if (index < (picsList.length - 1)) {
-          newrow = true;
-          //if (row == 2) {
-          if (row == 1) {
-            slide = true;
-            row = 0;
-          }
-        }
-      }
-      col++;
-  }
-}
-
-function smallscreenPics() {
-  var smallscreenDiv = document.getElementById("smallscreen");
-
-  for (let index = 0; index < picsList.length; ++index) {
-
-    slideDiv = document.createElement("div");
-    slideDiv.className += "slideDiv_small fade";
-    smallscreenDiv.appendChild(slideDiv);
-
-    var anchor = document.createElement('a');
-    anchor.setAttribute('href', picsList[index][1]);
-    slideDiv.appendChild(anchor);
-
-    //Image
-    var image = document.createElement("img");
-    image.className += picsList[index][2];
-    image.setAttribute("src", picsList[index][3]);
-    image.setAttribute("alt", picsList[index][4]);
-    image.setAttribute("style", picsList[index][5]);
-
-    anchor.appendChild(image);
-
-  }
-
 }
