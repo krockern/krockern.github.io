a07cbf5d33b4f566c48ac1a25b4a5eaff145a157
krockern <hoglund.c@gmail.com>
2018-09-17 21:35:35 +0200

    Cont. test of forms

--- php/send_mail.php
+++ php/send_mail.php
@@ -1,20 +1,21 @@
 <meta charset="utf-8">
 <?php
-echo phpversion();
+
 if(isset($_POST['email'])) {
 
 
-
+    $emailError = '';
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "test@nasvall.dodwiki.se";
-    $email_subject = "Kontaktformulär: ";
+    //$email_to = "test@nasvall.dodwiki.se";
+    $email_to = "test@mail.supernus.se";
+    //$email_subject = "Kontaktbegäran från: ";
 
     function died($error) {
         // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
+        echo "Tyvärr så har något blivit fel med formuläret.<br />";
+        echo "Nedan finns felen.<br /><br />";
         echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
+        echo "Vänligen gå tillbaka och skriv in giltig data.<br /><br />";
         die();
     }
 
@@ -30,13 +31,13 @@ if(isset($_POST['email'])) {
 
 
     $name = $_POST['name']; // required
-    echo "<br /><br />". $name . "<br />";
+    //echo "<br /><br />". $name . "<br />";
     $email_from = $_POST['email']; // required
-    echo $email_from . "<br />";
+    //echo $email_from . "<br />";
     $subject = $_POST['subject']; // not required
-    echo $subject . "<br />";
+    //echo $subject . "<br />";
     $comment = $_POST['comment']; // required
-    echo $comment . "<br /><br />";
+    //echo $comment . "<br /><br />";
 
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -44,6 +45,7 @@ if(isset($_POST['email'])) {
   //if(!preg_match($email_exp,$email_from)) {
   if (!filter_var($email_from, FILTER_VALIDATE_EMAIL)) {
     $error_message .= 'Ej korrekt inmatad epostadress.<br />';
+    $emailError = "Ej korrekt inmatad epostadress.";
     //$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
 
@@ -73,23 +75,78 @@ if(isset($_POST['email'])) {
   $email_message .= "Namn: ".clean_string($name)."<br />";
   $email_message .= "Epost: ".clean_string($email_from)."<br />";
   $email_message .= "Ämne: ".clean_string($subject)."<br />";
-  $email_message .= "Meddelande: ".clean_string($comment)."<br />";
+  $email_message .= "Meddelande: <br />".clean_string($comment)."<br />";
 
-  $email_subject .= $email_subject.$name;
+  $email_subject = "Kontaktbegäran från: ".$name;
 
   // create email headers
   $headers = "Content-Type: text/html; charset=UTF-8\r\n". 'From: '.$email_from."\r\n". 'Reply-To: '.$email_from."\r\n" . 'X-Mailer: PHP/' . phpversion();
 
 
-  echo "<br /><br />".$email_from."<br />".$email_to."<br />".$email_message;
-  //mail($email_from, $email_subject, $email_message, $headers);
-  $mailtest = mail($email_to, $email_subject, $email_message, $headers);
-  echo "<br /><br />".$mailtest."<br /><br />";
+  //echo "<br /><br />".$email_from."<br />".$email_to."<br />".$email_message;
+  mail($email_from, $email_subject, $email_message, $headers);
+  //$mailtest = mail($email_to, $email_subject, $email_message, $headers);
+  //echo "<br /><br />".$mailtest."<br /><br />";
 ?>
 
-<!-- include your own success html here -->
+<!DOCTYPE html>
+<html lang="sv">
+	<head>
+		<title>Nasvall early alpha</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+		<link rel="stylesheet" type="text/css" href="../css/w3.css">
+	</head>
 
-Tack för ditt meddelande.
+
+	<body>
+
+	<!-- Navbar (sit on top) -->
+	<div class="w3-top">
+		<div class="w3-bar w3-white w3-wide w3-padding w3-card">
+			<a href="http://nasvall.dodwiki.se/index.html" class="w3-bar-item w3-button"><b>Emil</b> Näsvall</a>
+			<!-- Float links to the right. Hide them on small screens -->
+			<div class="w3-right w3-hide-small">
+				<a href="http://nasvall.dodwiki.se/index.html" class="w3-bar-item w3-button">Tillbaka</a>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- Page content -->
+	<div class="w3-content w3-padding" style="max-width:1564px">
+
+		<!-- Project Section -->
+		<div class="w3-container w3-padding-32" id="projects">
+			<h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Tom sida</h3>
+		</div>
+
+		<!-- Row with pictures -->
+		<div class="w3-row-padding">
+
+
+
+			<div class="w3-col l6 m6 w3-margin-bottom">
+				<p>Tack för ditt meddelande. Vänligen gå tillbaka tillbaka till föregående sida genom att klicka <a href="http://nasvall.dodwiki.se/index.html">här</a></p>
+			</div>
+
+
+
+		</div>
+
+	<!-- End page content -->
+	</div>
+
+	<!-- Footer 	<footer class="w3-center w3-black w3-padding-16">-->
+		<!--<p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>-
+	</footer>-->
+
+</body>
+</html>
+
+
 
 <?php
 
